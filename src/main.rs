0915b054759cae8038173865343cbd682d21ec63
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-10-29 21:09:17 +0000

    write code 200 on any connection

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-// Uncomment this block to pass the first stage
+use std::io::prelude::*;
 use std::net::{SocketAddr, TcpListener, TcpStream, IpAddr, Ipv4Addr};
 
-fn handle_stream(_stream: TcpStream) {
-    println!("accepted new connection");
+fn handle_stream(mut stream: TcpStream) -> std::io::Result<usize> {
+    stream.read(&mut [0; 128])?;
+    stream.write(b"HTTP/1.1 200 OK\r\n\r\n")
 }
 
 fn main() -> std::io::Result<()> {
     let localhost = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4221);
     let listener = TcpListener::bind(localhost)?;
     for stream in listener.incoming() {
-        handle_stream(stream?);
+        _ = handle_stream(stream?);
     }
     Ok(())
 }
