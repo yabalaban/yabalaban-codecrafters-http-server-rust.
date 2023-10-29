4ea31834feb18f4abe5dbef348550991bd5e5de4
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-10-29 21:40:09 +0000

    parse request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,59 @@
+use std::collections::HashMap;
 use std::io::prelude::*;
 use std::net::{SocketAddr, TcpListener, TcpStream, IpAddr, Ipv4Addr};
 
+const CRLF: &str = "\r\n";
+
+enum HTTPMethod {
+    GET
+}
+
+struct HTTPRequest {
+    method: HTTPMethod,
+    path: String,
+    version: String,
+    headers: HashMap<String, String>,
+}
+
+fn parse_request_str(raw: String) -> Result<HTTPRequest, ()> {
+    let lines = raw.split(CRLF)
+        .collect::<Vec<&str>>();
+    assert!(lines.len() > 1);
+    let start_line = lines[0].split(' ')
+        .collect::<Vec<&str>>();
+    assert!(start_line.len() == 3);
+
+    let mut headers = HashMap::new();
+    if start_line.len() > 2 {
+        for line in start_line[2..].into_iter() {
+            let header = line.split(": ")
+                .collect::<Vec<&str>>();
+            assert!(header.len() == 2);
+            headers.insert(header[0].to_string(), header[1].to_string());
+        }
+    }
+    let request = HTTPRequest {
+        method: HTTPMethod::GET,
+        path: start_line[1].to_string(),
+        version: start_line[2].to_string(),
+        headers: headers
+    };
+    Ok(request)
+}
+
+fn write_response_code(mut stream: TcpStream, status: &str) -> std::io::Result<usize> {
+    let response = format!("HTTP/1.1 {status} {CRLF}{CRLF}");
+    stream.write(response.as_bytes())
+}
+
 fn handle_stream(mut stream: TcpStream) -> std::io::Result<usize> {
-    stream.read(&mut [0; 128])?;
-    stream.write(b"HTTP/1.1 200 OK\r\n\r\n")
+    let mut request_str = String::new(); 
+    stream.read_to_string(&mut request_str)?;
+    let request = parse_request_str(request_str).unwrap();
+    match request.path.as_str() {
+        "/" => write_response_code(stream, "200 OK"),
+        _ => write_response_code(stream, "404 Not Found"),
+    }
 }
 
 fn main() -> std::io::Result<()> {
