4a39e8d58d5afa717ca6a1feb9bfecb95c25f265
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-10-31 22:31:53 +0000

    echo

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,75 @@ fn parse_request_str(raw: String) -> Result<HTTPRequest, ()> {
     Ok(request)
 }
 
-fn write_response_code(mut stream: TcpStream, status: &str) -> std::io::Result<usize> {
-    let response = format!("HTTP/1.1 {status} {CRLF}{CRLF}");
-    stream.write(response.as_bytes())
+enum HTTPResponseStatusCode {
+    Ok,
+    NotFound,
+}
+
+impl ToString for HTTPResponseStatusCode {
+    fn to_string(&self) -> String {
+        match &self {
+            HTTPResponseStatusCode::Ok => "200 OK".to_owned(),
+            HTTPResponseStatusCode::NotFound => "404 Not Found".to_owned(),
+        }
+    }
+}
+
+enum HTTPContentType {
+    PlainText
+}
+
+impl ToString for HTTPContentType {
+    fn to_string(&self) -> String {
+        match &self {
+            HTTPContentType::PlainText => "Content-Type: text/plain".to_owned(),
+        }
+    }
+}
+
+struct HTTPResponsePayload { 
+    content_type: HTTPContentType,
+    payload: String,
+}
+
+impl ToString for HTTPResponsePayload {
+    fn to_string(&self) -> String {
+        let content_length = format!("Content-Length: {}", self.payload.len());
+        format!("{}\r\n{}\r\n\r\n{}", self.content_type.to_string(), content_length, self.payload)
+    }
+}
+
+struct HTTPResponse { 
+    status_code: HTTPResponseStatusCode,
+    payload: Option<HTTPResponsePayload>,
+}
+
+impl ToString for HTTPResponse {
+    fn to_string(&self) -> String {
+        let status = format!("HTTP/1.1 {}", self.status_code.to_string());
+        match &self.payload {
+            Some(val) => format!("{}\r\n{}", status, val.to_string()),
+            None => format!("{}\r\n\r\n", status),
+        }
+    }
+}
+
+fn handle_request(request: HTTPRequest) -> HTTPResponse {
+    match request.path.as_str() {
+        path if path.starts_with("/echo") => {
+            let components = path.split('/')
+                .collect::<Vec<&str>>();
+            HTTPResponse { 
+                status_code: HTTPResponseStatusCode::Ok, 
+                payload: Some(HTTPResponsePayload {
+                    content_type: HTTPContentType::PlainText,
+                    payload: components[2..].join("/").to_owned(),
+                }) 
+            }
+        },
+        "/" => HTTPResponse { status_code: HTTPResponseStatusCode::Ok, payload: None },
+        _ => HTTPResponse { status_code: HTTPResponseStatusCode::NotFound, payload: None },
+    }
 }
 
 fn handle_stream(mut stream: TcpStream) -> std::io::Result<usize> {
@@ -53,10 +119,9 @@ fn handle_stream(mut stream: TcpStream) -> std::io::Result<usize> {
     stream.read(&mut buffer)?;
     let request_str = String::from_utf8_lossy(&buffer).to_string();
     let request = parse_request_str(request_str).unwrap();
-    match request.path.as_str() {
-        "/" => write_response_code(stream, "200 OK"),
-        _ => write_response_code(stream, "404 Not Found"),
-    }
+    let response = handle_request(request);
+    println!("{}", response.to_string());
+    stream.write(response.to_string().as_bytes())
 }
 
 fn main() -> std::io::Result<()> {
