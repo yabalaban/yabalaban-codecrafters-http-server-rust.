a5c475cf97aaa85f28777aed838dbf125da202e9
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-11-01 21:11:01 +0000

    post file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
+use lazy_static::lazy_static;
+
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io::prelude::*;
 use std::path::Path;
+use std::str::FromStr;
+use std::sync::Mutex;
+
 use std::thread;
 use std::net::{SocketAddr, TcpListener, TcpStream, IpAddr, Ipv4Addr};
 
 const CRLF: &str = "\r\n";
 
+/*  Disclaimer: bad-quality code is expected, I had no intention to do things right but to hack around with Rust. */
+
+// Hacky workaround for global context
+
+#[derive(Clone)]
+struct ExecutionContext {
+    directory: Option<String>
+}
+
+lazy_static! {
+    static ref GLOBAL_CONTEXT: Mutex<ExecutionContext> = Mutex::new(ExecutionContext { directory: None });
+}
+
+fn get_context() -> ExecutionContext {
+    GLOBAL_CONTEXT.lock().unwrap().clone()
+}
+
+fn set_context(context: ExecutionContext) {
+    *GLOBAL_CONTEXT.lock().unwrap() = context
+}
+
+// Some HTTP internals and fancy structs
+#[derive(PartialEq)]
 enum HTTPMethod {
-    GET
+    GET,
+    POST,
+}
+
+impl FromStr for HTTPMethod {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(HTTPMethod::GET),
+            "POST" => Ok(HTTPMethod::POST),
+            _ => Err(()),
+        }
+    }
 }
 
 struct HTTPRequest {
     method: HTTPMethod,
     path: String,
-    version: String,
     headers: HashMap<String, String>,
+    body: String,
 }
 
 fn parse_request_str(raw: String) -> Result<HTTPRequest, ()> {
@@ -28,27 +69,37 @@ fn parse_request_str(raw: String) -> Result<HTTPRequest, ()> {
     assert!(start_line.len() == 3);
 
     let mut headers = HashMap::new();
+    let mut body = String::new();
+    let mut append_body = false;
+
     if lines.len() > 2 {
         for line in lines[2..].into_iter() {
-            if line.len() == 0 { continue; }
-            let header = line.split(": ")
-                .collect::<Vec<&str>>();
-            if header.len() == 2 {
-                headers.insert(header[0].to_string(), header[1].to_string());
+            if line.len() == 0 { append_body = true; continue; }
+            if append_body {
+                body.push_str(line.to_owned().trim_end_matches(char::from(0)));
+            } else {
+                let header = line.split(": ")
+                    .collect::<Vec<&str>>();
+                if header.len() == 2 {
+                    headers.insert(header[0].to_string(), header[1].to_string());
+                }
             }
         }
     }
+
+    let method = HTTPMethod::from_str(start_line[0]).unwrap();
     let request = HTTPRequest {
-        method: HTTPMethod::GET,
+        method: method,
         path: start_line[1].to_string(),
-        version: start_line[2].to_string(),
-        headers: headers
+        headers: headers,
+        body: body,
     };
     Ok(request)
 }
 
 enum HTTPResponseStatusCode {
     Ok,
+    Created,
     NotFound,
 }
 
@@ -56,6 +107,7 @@ impl ToString for HTTPResponseStatusCode {
     fn to_string(&self) -> String {
         match &self {
             HTTPResponseStatusCode::Ok => "200 OK".to_owned(),
+            HTTPResponseStatusCode::Created => "201 Created".to_owned(),
             HTTPResponseStatusCode::NotFound => "404 Not Found".to_owned(),
         }
     }
@@ -102,10 +154,46 @@ impl ToString for HTTPResponse {
     }
 }
 
-fn handle_request(request: HTTPRequest, context: Option<ServerContext>) -> HTTPResponse {
-    match request.path.as_str() {
-        path if path.starts_with("/echo") => {
-            let components = path.split('/')
+// Input stream handling
+
+#[derive(Clone, Copy)]
+struct RequestHandler {
+    predicate: fn(&HTTPRequest) -> bool,
+    handle: fn(&HTTPRequest) -> HTTPResponse,
+}
+
+fn handle_request(request: HTTPRequest, handlers: &[RequestHandler]) -> HTTPResponse {
+    let handler = handlers.into_iter().find(|h| (h.predicate)(&request));
+    match handler {
+        Some(h) => (h.handle)(&request),
+        None => HTTPResponse { status_code: HTTPResponseStatusCode::NotFound, payload: None },
+    }
+}
+
+fn handle_stream(mut stream: TcpStream, handlers: &[RequestHandler]) -> std::io::Result<usize> {
+    let mut buffer = [0; 2048];
+    stream.read(&mut buffer)?;
+    let request_str = String::from_utf8_lossy(&buffer).to_string();
+    let request = parse_request_str(request_str).unwrap();
+    let response = handle_request(request, handlers);
+    stream.write(response.to_string().as_bytes())
+}
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let localhost = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4221);
+    let listener = TcpListener::bind(localhost)?;
+
+    if args.len() > 1 && args[1] == "--directory" {
+        let mut context = get_context();
+        context.directory = Some(args[2].clone());
+        set_context(context);
+    } 
+
+    let echo = RequestHandler {
+        predicate: |request| request.path.starts_with("/echo"),
+        handle: |request| {
+            let components = request.path.split('/')
                 .collect::<Vec<&str>>();
             HTTPResponse { 
                 status_code: HTTPResponseStatusCode::Ok, 
@@ -114,8 +202,12 @@ fn handle_request(request: HTTPRequest, context: Option<ServerContext>) -> HTTPR
                     payload: components[2..].join("/").to_owned(),
                 }) 
             }
-        },
-        path if path.starts_with("/user-agent") => {
+        }
+    };
+
+    let user_agent = RequestHandler {
+        predicate: |request| request.path.starts_with("/user-agent"),
+        handle: |request| {
             HTTPResponse { 
                 status_code: HTTPResponseStatusCode::Ok, 
                 payload: Some(HTTPResponsePayload {
@@ -123,62 +215,62 @@ fn handle_request(request: HTTPRequest, context: Option<ServerContext>) -> HTTPR
                     payload: request.headers.get("User-Agent").unwrap().to_owned(),
                 }) 
             }
-        },
-        path if path.starts_with("/files") => {
-            let components = path.split('/')
-                .collect::<Vec<&str>>();
+        }
+    };
+
+    let main = RequestHandler {
+        predicate: |request| request.path == "/",
+        handle: |_request| HTTPResponse { status_code: HTTPResponseStatusCode::Ok, payload: None },
+    };
+
+    let get_file = RequestHandler {
+        predicate: |request| request.method == HTTPMethod::GET && request.path.starts_with("/files"),
+        handle: |request| { 
+            let components = request.path.split('/').collect::<Vec<&str>>();
             let filename = components[2].to_owned();
-            let filepath = format!("{}{}", context.unwrap().directory, filename);
+            let context = get_context();
+            let filepath = format!("{}{}", context.directory.unwrap(), filename);
             if Path::new(filepath.as_str()).exists() { 
                 HTTPResponse { 
                     status_code: HTTPResponseStatusCode::Ok, 
                     payload: Some(HTTPResponsePayload {
                         content_type: HTTPContentType::ApplicationOctetStream,
                         payload: fs::read_to_string(filepath).unwrap().parse().unwrap(),
-                    }) 
+                    })
                 }
             } else {
                 HTTPResponse { status_code: HTTPResponseStatusCode::NotFound, payload: None }
             }
-        },
-        "/" => HTTPResponse { status_code: HTTPResponseStatusCode::Ok, payload: None },
-        _ => HTTPResponse { status_code: HTTPResponseStatusCode::NotFound, payload: None },
-    }
-}
+        }
+    };
 
-fn handle_stream(mut stream: TcpStream, context: Option<ServerContext>) -> std::io::Result<usize> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?;
-    let request_str = String::from_utf8_lossy(&buffer).to_string();
-    let request = parse_request_str(request_str).unwrap();
-    let response = handle_request(request, context);
-    stream.write(response.to_string().as_bytes())
-}
+    let post_file = RequestHandler {
+        predicate: |request| request.method == HTTPMethod::POST && request.path.starts_with("/files"),
+        handle: |request| { 
+            let components = request.path.split('/').collect::<Vec<&str>>();
+            let filename = components[2].to_owned();
+            let context = get_context();
+            let filepath = format!("{}{}", context.directory.unwrap(), filename);
+            fs::write(filepath, request.body.clone()).expect("should be fine"); 
+            HTTPResponse { 
+                status_code: HTTPResponseStatusCode::Created, 
+                payload: None
+            }
+        }
+    };
 
-#[derive(Copy, Clone)]
-struct ServerContext {
-    directory: &'static str,
-}
-
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    let localhost = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4221);
-    let listener = TcpListener::bind(localhost)?;
-    let context: Option<ServerContext>;
-    if args.len() > 1 && args[1] == "--directory" {
-        let directory: &'static str = Box::leak(args[2].clone().into_boxed_str());
-        context = Some(ServerContext {
-            directory: directory
-        });
-    } else {
-        context = None;
-    }
+    let handlers = [
+        echo,
+        user_agent,
+        get_file,
+        post_file,
+        main,
+    ];
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream_) => _ = thread::spawn(move || {
-                _ = handle_stream(stream_, context);
+                _ = handle_stream(stream_, &handlers);
             }),
             Err(e) => println!("{}", e)   
         }
